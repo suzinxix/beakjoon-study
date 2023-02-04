61441013ee5e46b9e6d193c6f4db6a8666b13a71
gabi <70315572+Limgabi@users.noreply.github.com>
2023-02-04 15:51:24 +0900

    2750: 수 정렬하기

---
+++ Week01/gabi/2750.js
@@ -0,0 +1,39 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+const arr = input.slice(1);
+
+let ans = '';
+
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0, j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+const result = mergeSort(arr);
+ans = result.join('\n');
+console.log(ans);
