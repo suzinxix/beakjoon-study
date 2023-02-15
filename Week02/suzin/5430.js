095468f4a7031ef99945b3c557413f59cef4114d
suzinxix <suzinxix@gmail.com>
2023-02-15 22:42:53 +0900

    5430: AC

---
+++ Week02/suzin/5430.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+// TODO: 제출 시 경로 변환 필수 ("/dev/stdin")
+const input = fs.readFileSync("input.txt").toString().trim().split("\n");
+const N = Number(input.shift());
+let answer = "";
+for (let i = 0; i < N; i++) {
+  let testcase = input.splice(0, 3);
+  let isReverse = false;
+  testcase[2] = testcase[2].slice(1, -1).split(",");
+
+  for (let p of testcase[0]) {
+    var flag = true;
+
+    if (p == "R") isReverse = !isReverse;
+    else if (p == "D") {
+      if (testcase[1] == "0") {
+        answer += "error\n";
+        flag = !flag;
+        break;
+      }
+      if (testcase[2].length == 0) {
+        answer += "error\n";
+        flag = !flag;
+        break;
+      }
+      if (isReverse) testcase[2].pop();
+      else testcase[2].shift();
+    }
+  }
+  if (isReverse) testcase[2].reverse();
+  if (flag) answer += "[" + testcase[2].join(",") + "]\n";
+}
+
+console.log(answer.trim());
